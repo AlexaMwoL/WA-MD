7aa19807ca0ff5c2eb589360eb9d08d8ebfc04ac
TEAM ALEXA <93711729+AlexaMwoL@users.noreply.github.com>
2022-07-31 20:08:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 
 global.prefix = '+'
 global.mode = 'publik'
-global.packName = 'Sticker'
-global.authorName = 'Bot'
+global.packName = '🎈K R I S H N A D A S🎈'
+global.authorName = '⚡️T O X I C⚡️'
 // global.thumb = fs.readFileSync('./thumb.jpeg')
-global.ownerNumber = ['0', '+62 877-7456-95315', '+62 877-7456-95317', '+62 813-8283-6249']
+global.ownerNumber = ['0', '919633687665', '917306398363', '917736622139']
 global.thumbnailUrl = ['https://telegra.ph/file/69d52042b39386c222151.jpg', 'https://telegra.ph/file/7079e0b324406d3d7543c.jpg', 'https://telegra.ph/file/3a3edf8916e3239522a34.jpg']
 // global.db = JSON.parse(fs.readFileSync('./db.json'))
 
